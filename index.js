5b11a73eeaef96f3472ef0d04e254d4845f0cd27
YouJin Lee <dev.ujin518@gmail.com>
2021-03-01 12:48:06 +0100

    [refactor] Hide secret information of database connection

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const bodyParser = require('body-parser')
+const config = require("./config/key")
+const {User} = require("./models/User") 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://<username>:<password>@<cluster>.porkr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
@@ -11,7 +19,17 @@ mongoose.connect('mongodb+srv://<username>:<password>@<cluster>.porkr.mongodb.ne
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-}) //root 디렉토리에 오면 Hello World를 출력하겠다.
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, doc) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- models/User.js
+++ models/User.js
@@ -31,6 +31,6 @@ const userSchema = mongoose.Schema({
     }
 })
 
-const User = monoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema)
 
 module.exports = {User}
